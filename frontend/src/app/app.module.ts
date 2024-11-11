ef4e821a0af75978b8ee7859f6e2af6bfbb66800
DewaSRY <sdewa6645@gmail.com>
2024-11-11 07:08:54 +0700

    add:setup global interceptro

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {provideHttpClient, withInterceptors} from "@angular/common/http"
 
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
 import {MAT_FORM_FIELD_DEFAULT_OPTIONS } from "@angular/material/form-field"
 
-import {provideHttpClient} from "@angular/common/http"
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { credentialsInterceptor } from "@app/core/interceptors/credentials.interceptor"
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,7 +23,9 @@ import {provideHttpClient} from "@angular/common/http"
   ],
   bootstrap: [AppComponent],
   providers: [
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([credentialsInterceptor])
+    ),
     {
       provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
       useValue : {apperance: "outline", subscriptSizing: "dynamic"}
