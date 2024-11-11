6617b1ef45b9b46b4a5d212f746b4a45934e2a1d
DewaSRY <sdewa6645@gmail.com>
2024-11-11 11:15:46 +0700

    add:make new type for auth query

--- frontend/src/app/auth/services/auth-query.service.ts
+++ frontend/src/app/auth/services/auth-query.service.ts
@@ -1,20 +1,21 @@
-import { DestroyRef, inject, Injectable } from '@angular/core';
-import { AuthService } from './auth.service';
+import { DestroyRef, Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { BehaviorSubject, map, Observable } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+export type AuthQueryParam="login" | "signup"
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthQueryService {
 
-  readonly authParam = new BehaviorSubject<"login" | "signup">("login")
-  
+  readonly authParam = new BehaviorSubject<AuthQueryParam>("login")
+
   constructor(activeRouter:ActivatedRoute, destroyRef: DestroyRef) {
     activeRouter.queryParamMap
       .pipe(takeUntilDestroyed(destroyRef))
-      .pipe(map(data=> data.get("auth") as "login" | "signup" || "login" ))
+      .pipe(map(data=> data.get("auth") as AuthQueryParam || "login" ))
       .subscribe((data)=> this.authParam.next(data))
   }
 
