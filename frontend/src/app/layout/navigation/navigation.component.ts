193df02be8a89fc42e145d7348aa02094a6840c5
DewaSRY <sdewa6645@gmail.com>
2024-11-11 07:07:16 +0700

    add:update navigation

--- frontend/src/app/layout/navigation/navigation.component.ts
+++ frontend/src/app/layout/navigation/navigation.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
+
+import { AuthService } from '@app/auth/services/auth.service';
+import { UserResponse } from '@app/auth/model/auth';
+import { BehaviorSubject, catchError, EMPTY, filter, map, Observable  } from 'rxjs'
+import { ActivatedRoute } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 
 @Component({
   selector: 'app-navigation',
@@ -6,5 +12,50 @@ import { Component } from '@angular/core';
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
+  
+  
+  private readonly services = {
+    authServices: inject(AuthService),
+    activeRouter: inject(ActivatedRoute),
+    destroyRef: inject(DestroyRef)
+  }
+
+  auth$= new BehaviorSubject(false)
+  
+  authLink : "login" | "signup" = "login"
+ 
+  ngOnInit(): void {
+    this.setupParamObserver()
+    this.setupSigninObserver()
+  }
+
+  private  setupParamObserver() {
+    const { destroyRef, activeRouter } = this.services
+    activeRouter.queryParamMap
+      .pipe(
+        takeUntilDestroyed(destroyRef),
+        map(param => param.get("auth") as "login" | "signup" || "login")
+      )
+      .subscribe({
+        next: data => { this.authLink= data }
+    })
+  } 
+
+  private setupSigninObserver() {
+    const { destroyRef, authServices } = this.services
+    authServices.getUserSignedin()
+      .pipe(takeUntilDestroyed(destroyRef))
+      .subscribe({
+        next: () => { this.auth$.next(true) },
+        error: ()=>{ this.auth$.next(false) }
+      })
+  }
+
+  logout() {
+    const { destroyRef, authServices } = this.services
+    authServices.deleteLogout()
+      .pipe(takeUntilDestroyed(destroyRef))
+      .subscribe(()=> this.auth$.next(false))
+  }
 
 }
