608f9fb8233a74c8036a7775e0b142d0038d0d1f
DewaSRY <sdewa6645@gmail.com>
2024-11-12 14:11:13 +0700

    update:simplify auth services

--- frontend/src/app/auth/model/auth.ts
+++ frontend/src/app/auth/model/auth.ts
@@ -7,23 +7,18 @@ export interface SignupPyload extends LoginPyload{
     name: string
 }
 
-export interface UserResponse{
-    data: {
-        id: string,
-        name: string,
-        email: string,
-        created_at: string, 
-        updated_at: string
-    }
+export interface User{
+    id: string,
+    name: string,
+    email: string,
+    created_at: string, 
+    updated_at: string
 }
-export interface AuthResponse{
+
+export interface UserResponse{
+    data:User
+}
+export interface AuthResponse extends UserResponse{
     token: string
-    data: {
-        id: string,
-        name: string,
-        email: string,
-        created_at: string, 
-        updated_at: string
-    }
 }
 
--- frontend/src/app/auth/services/auth.service.ts
+++ frontend/src/app/auth/services/auth.service.ts
@@ -2,7 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from "@environments/environment"
 
-import { AuthResponse, LoginPyload, SignupPyload, UserResponse } from "@app/auth/model/auth"
+import { AuthResponse, LoginPyload, SignupPyload, UserResponse,User } from "@app/auth/model/auth"
+import { BehaviorSubject, map, Observable, of, tap } from 'rxjs';
 
 export type Property=  "name" | 'email'
 export interface UniqProperty{
@@ -18,29 +19,46 @@ export interface UniquePropertyResponse{ is_unique: boolean}
 })
 export class AuthService {
   private readonly endpoint = `${environment.domain}/user`
-  
+  user$= new BehaviorSubject<User| null>(null)
   private readonly services = {
     httpClient: inject(HttpClient)
   }
 
-  getUserSignedin() {
-    const {httpClient} = this.services
-    return httpClient.get<UserResponse>(this.endpoint+ "/signin")
+  getUserSignedin(): Observable<User> {
+    const { httpClient } = this.services
+    if (this.user$.value !== null) {
+      return of(this.user$.value)
+    }
+    return httpClient.get<UserResponse>(this.endpoint + "/signin")
+      .pipe(map(response=> response.data))
+      .pipe(tap(response => this.user$.next(response)))
+        
   }
   
-  postUserRegister(pyload: SignupPyload) {
-    const {httpClient} = this.services
-    return httpClient.post<AuthResponse>(this.endpoint+ "/signup", pyload)
+  postUserRegister(pyload: SignupPyload): Observable<User> {
+    const { httpClient } = this.services
+    if (this.user$.value !== null) {
+      return of(this.user$.value)
+    }
+    return httpClient.post<AuthResponse>(this.endpoint + "/signup", pyload)
+      .pipe(map(response=> response.data))
+      .pipe(tap(response => this.user$.next(response)))
+  }
+
+  postUserLogin(pyload: LoginPyload): Observable<User> {
+    const { httpClient } = this.services
+    if (this.user$.value !== null) {
+      return of(this.user$.value)
+    }
+    return httpClient.post<AuthResponse>(this.endpoint + "/login", pyload)
+      .pipe(map(response=> response.data))
+      .pipe(tap(response => this.user$.next(response)))
   }
 
   deleteLogout() {
     const {httpClient} = this.services
-    return httpClient.delete<void>(this.endpoint+ "/logout")
-  }
-
-  postUserLogin(pyload: LoginPyload) {
-    const {httpClient} = this.services
-    return httpClient.post<AuthResponse>(this.endpoint+ "/login", pyload)
+    return httpClient.delete<void>(this.endpoint + "/logout")
+      .pipe(tap(()=>this.user$.next(null)))
   }
 
   postUniqueUserProperty(pyload: UniqProperty) {
