040663cfe5f365315378b590191b227da379f320
DewaSRY <sdewa6645@gmail.com>
2024-11-13 16:29:40 +0700

    init:define calender model

--- frontend/src/app/todo/model/heat-map-calender.ts
+++ frontend/src/app/todo/model/heat-map-calender.ts
@@ -7,9 +7,9 @@ export interface CalendarData{
     series: Series[]
     name: string
 }
-
+export type Cuartel=  "Q1" | "Q2" | "Q3" | "Q4";
 
 export interface HeatMapFilter{
-  cuartel: "Q1" | "Q2" | "Q3" | "Q4";
+  cuartel: Cuartel;
   year: string
 }
