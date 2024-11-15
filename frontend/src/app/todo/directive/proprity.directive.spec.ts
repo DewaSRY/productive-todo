c538842825b51d6c535317e2f436b360278823e8
DewaSRY <sdewa6645@gmail.com>
2024-11-15 11:23:38 +0700

    add:create priority directive

---
+++ frontend/src/app/todo/directive/proprity.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ProprityDirective } from './proprity.directive';
+
+describe('ProprityDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ProprityDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/todo/directive/proprity.directive.ts
@@ -0,0 +1,47 @@
+import { Directive, Input, ElementRef, Renderer2, inject } from '@angular/core';
+import { Priority } from '@app/todo/model/todo'
+
+@Directive({
+  selector: '[appProprity]',
+})
+export class ProprityDirective {
+
+  @Input() set appProprity(priority: Priority) {
+    this.setTailwindClassesBasedOnPriority(priority);
+  }
+
+  private readonly services = {
+    el: inject(ElementRef),
+    renderer: inject(Renderer2)
+  }
+
+  private setTailwindClassesBasedOnPriority(priority: Priority): void {
+    const { el, renderer } = this.services
+
+    renderer.addClass(el.nativeElement, 'text-slate-700');
+    renderer.addClass(el.nativeElement, 'extra-small');
+    renderer.addClass(el.nativeElement, 'hover:brightness-50');
+    renderer.addClass(el.nativeElement, 'duration-300');
+    renderer.addClass(el.nativeElement, 'ease-in-out');
+    renderer.addClass(el.nativeElement, 'shadow-2xl');
+
+    switch(priority) {
+      case 'HIGHT':
+        renderer.addClass(el.nativeElement, 'bg-priority-one/70');
+        break;
+      case 'MID':
+        renderer.addClass(el.nativeElement, 'bg-priority-two/70');
+        break;
+      case 'LOW':
+        renderer.addClass(el.nativeElement, 'bg-priority-three/70');
+        break;
+      default:
+        renderer.addClass(el.nativeElement, 'border');
+        renderer.addClass(el.nativeElement, 'border-text/10');
+        renderer.addClass(el.nativeElement, 'bg-slate-700/10');
+        break;
+    }
+
+  }
+
+}
