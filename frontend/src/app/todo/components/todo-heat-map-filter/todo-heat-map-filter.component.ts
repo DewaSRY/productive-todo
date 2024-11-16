5f94ebb85a528ae1a6610a9c1d4c0c9547d06040
DewaSRY <sdewa6645@gmail.com>
2024-11-16 07:26:02 +0700

    update:finishes clean up the dashboard

--- frontend/src/app/todo/components/todo-heat-map-filter/todo-heat-map-filter.component.ts
+++ frontend/src/app/todo/components/todo-heat-map-filter/todo-heat-map-filter.component.ts
@@ -2,7 +2,7 @@ import { Component, DestroyRef, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Cuartel } from "@app/todo/model/heat-map-calender"
 import { TodoHeatMapService } from '@app/todo/services/todo-heat-map.service';
-import { tap } from 'rxjs';
+
 @Component({
   selector: 'app-todo-heat-map-filter',
   templateUrl: './todo-heat-map-filter.component.html',
@@ -20,7 +20,6 @@ export class TodoHeatMapFilterComponent  {
     const {heatMapServices,destroyRef } = this.services
     heatMapServices.currectCuartal$
       .pipe(takeUntilDestroyed(destroyRef))
-      .pipe(tap(d=> console.log(d)))
       .subscribe((c) => this.currentCuartal = c)
   }
 
